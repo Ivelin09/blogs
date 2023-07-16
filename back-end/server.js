f5f865b112a2ab586ec62cb15131853b84b5b9d5
Ivelin09 <galinaliobomirova@gmail.com>
2023-07-16 22:11:53 +0300

    Back-End Login users

--- back-end/server.js
+++ back-end/server.js
@@ -43,7 +43,25 @@ server.post('/register', async (req, res) => {
         message: "success",
         token: token
     });
+});
 
-    server.listen(8000, () => {
-        console.log('Server is on')
-    })
+server.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const query = await User.findOne({ username: username });
+    console.log(query);
+
+    if (!query || query.password != password)
+        res.json({ message: 'wrong username or password', code: 403 });
+    else
+        res.json({
+            message: 'success',
+            token: await jwt.sign({ username: username }, 'secret', { expiresIn: 1000 * 60 * 30 }),
+            status: 200
+        });
+    console.log(query);
+});
+
+server.listen(8000, () => {
+    console.log('Server is on')
+})
