96b7a5a0a3978f40aa4a31bc70f11cee1412e9c9
Ivelin09 <galinaliobomirova@gmail.com>
2023-07-17 17:35:39 +0300

    user schema seperated

---
+++ back-end/schemas/users.js
@@ -0,0 +1,16 @@
+const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: String
+});
+
+
+const User = mongoose.model('Users', userSchema);
+
+module.exports = User;
