e4ffc5b2172a91f150e6c7f6f49dc49592dec87f
Ivelin09 <galinaliobomirova@gmail.com>
2023-07-24 23:54:51 +0300

    Front End credentials sent with socket

--- front-end/src/socket.js
+++ front-end/src/socket.js
@@ -1,3 +1,5 @@
 import { io } from 'socket.io-client';
 
-export const socket = io(`${process.env.REACT_APP_PROXY_SERVER}`);
+export const socket = io(`${process.env.REACT_APP_PROXY_SERVER}`, {
+    withCredentials: true
+});
