da39a463e1614cfbe70386e0a95bd5d0f415858d
Ivelin09 <galinaliobomirova@gmail.com>
2023-07-24 23:54:30 +0300

    Delete comment Route added

--- back-end/routes/comment.route.js
+++ back-end/routes/comment.route.js
@@ -4,6 +4,7 @@ const authorize = require('../middleware/auth');
 const commentController = require('../controllers/comment');
 
 router.post('/comment', authorize, commentController.post);
+router.delete('/comment/:id', authorize, commentController.delete);
 router.get('/reply/:commentId', commentController.getOne);
 router.get('/comments/:blogId', commentController.getAll);
 
