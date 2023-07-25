b615205b917bb6462aad112caec8f3224184a335
Ivelin09 <galinaliobomirova@gmail.com>
2023-07-25 23:49:49 +0300

    js -> jsx

---
+++ front-end/src/Router.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import CreateBlog from './pages/createBlog'
+import Register from './pages/register'
+import Login from './pages/login';
+import Blogs from './pages/blogs'
+import Blog from './pages/blog'
+
+import { socket } from './socket';
+
+function Router() {
+  const [isConnected, setConnection] = useState(false);
+  useEffect(() => {
+
+    const establishConnection = () => {
+      socket.emit('connection');
+    }
+
+    socket.on('connected', () => {
+      setConnection(true);
+    });
+
+    const heartbeat = setInterval(() => {
+      console.log('is it working??');
+      socket.emit('online');
+    }, 3000);
+
+
+    establishConnection();
+
+    return () => {
+      socket.off('connected');
+      window.removeEventListener('beforeunload', () => {
+        socket.close();
+      })
+
+      clearInterval(heartbeat);
+    }
+  }, []);
+  return (
+    <div className="App">
+      {isConnected &&
+        <BrowserRouter>
+          <Routes >
+            <Route path="/createBlog" element={<CreateBlog />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
+        </BrowserRouter>}
+    </div>
+  );
+}
+
+export default Router;
